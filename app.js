afd9798e25def85274c641981ba05be9c0507f59
Christian Lisangola <jochri3@gmail.com>
2024-02-13 16:24:32 +0300

    Events and styles

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const technos = document.querySelectorAll("li");
+// const techno = document.querySelector("li");
 
-// console.log(technos[0].innerText);
-
-// for (let i = 0; i < technos.length; i++) {
-//   console.log(technos[i].innerText);
+// Bad practice
+// for (const techno of technos) {
+//   techno.style.backgroundColor = "indigo";
+//   techno.style.fontSize = "20px";
+//   techno.style.border = "1px solid orangered";
+//   techno.style.color = "orangered";
 // }
 
 for (const techno of technos) {
-  console.log(techno.innerText);
-  // techno.innerText = "J'ai faim";
+  techno.addEventListener("click", function () {
+    // console.log(techno.innerText);
+    console.log(this.innerText);
+    // this.classList.add("tech");
+
+    // if (this.classList.contains("tech")) {
+    //   this.classList.remove("tech");
+    // } else {
+    //   this.classList.add("tech");
+    // }
+    this.classList.toggle("tech");
+  });
 }
 
 const framworks = [
@@ -21,13 +34,3 @@ const framworks = [
   "Laravel",
   "Dynamo",
 ];
-
-// TP1
-// Remplacer tous les elements du DOM avec le tableaux ci-dessus
-
-// TP2
-// Dans l'affichage, vous devez avoir "nom_de_classe - technologie"
-// Exemple :
-//   Frontend - HTML
-//   Server side - Nest.js
-//   Database - Mongo DB
