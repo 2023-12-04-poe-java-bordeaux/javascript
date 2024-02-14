76b7ee665839138ef23f29cc0a312fcdfa232eee
Christian Lisangola <jochri3@gmail.com>
2024-02-14 10:38:33 +0300

    Alaram

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
-const technos = document.querySelectorAll("li");
-// const techno = document.querySelector("li");
+const alarmButton = document.querySelector("#alarm-button");
 
-// Bad practice
-// for (const techno of technos) {
-//   techno.style.backgroundColor = "indigo";
-//   techno.style.fontSize = "20px";
-//   techno.style.border = "1px solid orangered";
-//   techno.style.color = "orangered";
-// }
+const audio = new Audio("./alarm.wav");
 
-for (const techno of technos) {
-  techno.addEventListener("click", function () {
-    // console.log(techno.innerText);
-    console.log(this.innerText);
-    // this.classList.add("tech");
-
-    // if (this.classList.contains("tech")) {
-    //   this.classList.remove("tech");
-    // } else {
-    //   this.classList.add("tech");
-    // }
-    this.classList.toggle("tech");
-  });
-}
-
-const framworks = [
-  "Nest.js",
-  "Spring",
-  "Asp Dotnet Core",
-  "Ruby On Rails",
-  "Angular",
-  "Symfony",
-  "Laravel",
-  "Dynamo",
-];
+alarmButton.addEventListener("click", function () {
+  audio.play();
+});
