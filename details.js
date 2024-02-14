b155ce2b9c63e78cde3ff9a6f7d47e77d76a3fd8
Christian Lisangola <jochri3@gmail.com>
2024-02-14 15:48:15 +0300

    Finished first project

--- details.js
+++ details.js
@@ -1,5 +1,22 @@
+import users from "./users.js";
+
 const urlParams = new URLSearchParams(location.search);
 
-const id = urlParams.get("id");
+const id = parseInt(urlParams.get("id"));
+const user = users.find((user) => user.id === id);
 
-console.log(id);
+const $root = document.querySelector("#root");
+
+if (user) {
+  document.title = `Détails ${user.name}`;
+  $root.innerHTML = `
+    <h1>${user.name}</h1>
+    <h2><a href="mailto:${user.email}">${user.email}</a></h2>
+    <p><b>Company : </b>${user.company.name}</p>
+    <p>Phone : <a href="tel:${user.phone}">${user.phone}</a></p>
+`;
+} else {
+  $root.innerHTML = `<h1>404 Not Found</h1>
+    <p>User with id ${id} doesn't exist</p>
+  `;
+}
