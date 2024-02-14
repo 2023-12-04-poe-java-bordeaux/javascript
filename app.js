13cc4802c5ae071063a40ad29499d413918d5520
Christian Lisangola <jochri3@gmail.com>
2024-02-14 13:10:32 +0300

    Inner HTML

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const alarmButton = document.querySelector("#alarm-button");
+const framworks = [
+  "Nest.js",
+  "Spring",
+  "Asp Dotnet Core",
+  "Ruby On Rails",
+  "Angular",
+  "Symfony",
+  "Laravel",
+  "Dynamo",
+];
 
-const audio = new Audio("./alarm.wav");
+const technos = document.querySelector(".technos");
 
-alarmButton.addEventListener("click", function () {
-  audio.play();
+// for (const framwork of framworks) {
+//   technos.innerHTML += `<li>${framwork}</li>`;
+// }
+
+const addTechButton = document.querySelector("#add-tech");
+
+addTechButton.addEventListener("click", function () {
+  const elementToInsert = framworks.shift();
+  technos.innerHTML += `<li>${elementToInsert}</li>`;
+  console.log(framworks);
 });
